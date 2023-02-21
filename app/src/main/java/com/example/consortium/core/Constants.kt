1f3c053f7c5b164ab590772daedf105baad806b6
Sgascon-cstj <89391189+Sgascon-cstj@users.noreply.github.com>
2023-02-21 15:16:14 -0500

    Added base files

---
+++ app/src/main/java/com/example/consortium/core/Constants.kt
@@ -0,0 +1,4 @@
+package com.example.consortium.core
+
+object Constants {
+}
---
+++ app/src/main/java/com/example/consortium/presentation/Delivery/DeliveriesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.consortium.presentation.Delivery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.consortium.R
+
+class DeliveriesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deliveries)
+    }
+}
